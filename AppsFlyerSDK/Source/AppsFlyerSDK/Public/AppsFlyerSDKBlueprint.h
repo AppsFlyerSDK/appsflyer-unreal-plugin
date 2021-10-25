f24ff79de7cd7274bb6a8b043da515912c7c8243
Andrii Hahan <andrii.h@appsflyer.com>
2021-10-25 16:28:03 +0300

    Uninstall reporting for iOS

--- AppsFlyerSDK/Source/AppsFlyerSDK/Public/AppsFlyerSDKBlueprint.h
+++ AppsFlyerSDK/Source/AppsFlyerSDK/Public/AppsFlyerSDKBlueprint.h
@@ -44,4 +44,9 @@ class APPSFLYERSDK_API UAppsFlyerSDKBlueprint : public UBlueprintFunctionLibrary
 	 */
 	UFUNCTION(BlueprintCallable, Category = AppsFlyerSDK, DisplayName = "AppsFlyerSDK wait for user authorization")
 	static void waitForATTUserAuthorizationWithTimeoutInterval(int timeoutInterval);
+
+
+    UFUNCTION(BlueprintCallable, Category = AppsFlyerSDK, DisplayName = "Set AppsFlyerSDK Remote Notifications Token")
+    static void setRemoteNotificationsToken(const TArray<uint8>& token);
+
 };
